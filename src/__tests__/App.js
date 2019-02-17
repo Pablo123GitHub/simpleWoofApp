fe5d565d5f596a1d64d2eda531e2c4ee28e4a66c
Pablo <paul.ladiv123@gmail.com>
2019-02-17 11:56:34 +0000

    update tests

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -2,24 +2,30 @@ import mockAxios from 'axios'
 import ReactDOM from 'react-dom';
 import * as Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
-import { shallow} from 'enzyme';
+import { shallow, mount} from 'enzyme';
 import React from 'react';
+import { create } from 'react-test-renderer';
 
 describe('rendering', ()=> {
+    let wrapper;
+    beforeEach (()=>{
+         wrapper = shallow(<App/>)
+    })
 
-    it('componentDidMount is called once', async () => {
-
-       let wrapper = shallow(<App/>)
-        wrapper.update()
+    it('axios method "get" gets called once  right from the start', async () => {
+        // wrapper.find('button').simulate('click');
+        // the axios.get gets called twice because of the page update+ button click
         expect(mockAxios.get).toHaveBeenCalledTimes(1);
+     });
 
-    });
+    it('axios get gets called three times: first test + page update + click current test', async () => {
+        wrapper.find('button').simulate('click');
+        // the axios.get gets called twice because of the page update+ button click
+        expect(mockAxios.get).toHaveBeenCalledTimes(3);
+     });
+ 
 
-    it('componentDidMount to return value ', () => {
-
-        let wrapper = shallow(<App/>)
-        wrapper.update()
-
+    it('Axios returns a mocked value when axios.get is used', () => {
         mockAxios.get()
             .then(response => {
                 expect.assertions(1);
@@ -28,4 +34,12 @@ describe('rendering', ()=> {
             .catch(err => console.log('ERROR', err))
     });
 
+    it('componentsDidMount called from the start hence state == coolurl.png', () => {
+        const spy = jest.spyOn(App.prototype, 'componentDidMount')
+        const mountWrapper = mount(<App/>)
+        expect(spy).toHaveBeenCalled();
+        expect(wrapper.state('dog')).toEqual({"url": "urlwithcooldogs.png"});
+    });
+
 })
+
